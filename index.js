29a997000ddcbd63bc7a0d6d24ae3a8ff9a98fea
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2022-07-26 15:47:03 +0430

    test: GET /api/genres

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ require('./startup/db')();
 require('./startup/validation')();
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   winston.info(`Server is listening: http:///localhost:${port}/api/`);
 });
+
+module.exports = server;
